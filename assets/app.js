8cb7b9c27a6b445ed90e45b06e8a2c277327ede6
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2021-10-25 11:08:55 +0200

    Using bootstrap

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,17 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/root.css';
+import './styles/global.scss';
 import './styles/app.css';
+import './styles/navbar.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+const $ = require('jquery');
+require('bootstrap');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
